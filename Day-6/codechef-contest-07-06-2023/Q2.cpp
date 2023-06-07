e6f5231ea5d170c3dae0cb0478592166db3af587
Dipta-prog <anotheramit8@gmail.com>
2023-06-07 20:59:51 +0600

    - Q2 solved

---
+++ Day-6/codechef-contest-07-06-2023/Q2.cpp
@@ -0,0 +1,36 @@
+/*
+https://www.codechef.com/START93D/problems/SEVENRINGS
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isValidPhoneNumber(int number)
+{
+    string phoneNumber = to_string(number);
+    if (phoneNumber.length() != 5)
+        return false;
+
+    if (phoneNumber[0] == '0')
+        return false;
+
+    return true;
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int N, X;
+        cin >> N >> X;
+
+        int totalBill = N * X;
+
+        isValidPhoneNumber(totalBill) ? cout << "YES" << endl : cout << "NO" << endl;
+    }
+
+    return 0;
+}
