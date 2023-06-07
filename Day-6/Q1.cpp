019959f88ac55d6598647193bdfd1f0e5bcb93fc
Dipta-prog <anotheramit8@gmail.com>
2023-06-07 15:44:25 +0600

    - Q1 C - Finding Minimums Solved

---
+++ Day-6/Q1.cpp
@@ -0,0 +1,81 @@
+/*
+
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void findMinimumsInGroups(const vector<int> &input, int k)
+{
+    int n = input.size();
+    int i = 0;
+
+    while (i < n)
+    {
+        int minVal = input[i];
+
+        for (int j = i + 1; j < i + k && j < n; j++)
+            if (input[j] < minVal)
+                minVal = input[j];
+
+        cout << minVal << " ";
+        i += k;
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> numbers(N);
+    for (int i = 0; i < N; i++)
+        cin >> numbers[i];
+
+    findMinimumsInGroups(numbers, K);
+
+    return 0;
+}
+
+/*
+8 3
+4 -1 2 ----->-1
+3 5 0 ------> 0
+2 7 --------> 2
+*/
+
+//
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void findMinimumsInGroups(int N, int K)
+{
+    int minVal;
+    int input;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> input;
+
+        (i % K == 0) ? minVal = input : minVal = min(minVal, input);
+
+        if ((i + 1) % K == 0 || i == N - 1)
+            cout << minVal << " ";
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+
+    findMinimumsInGroups(N, K);
+
+    return 0;
+}
