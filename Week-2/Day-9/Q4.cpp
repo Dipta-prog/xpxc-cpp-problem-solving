4de243c8a0b743d0b0ba59441c66ab7a8be8dd4b
Dipta-prog <anotheramit8@gmail.com>
2023-06-10 20:33:41 +0600

    - Q4 B - How many? Solved

---
+++ Week-2/Day-9/Q4.cpp
@@ -0,0 +1,29 @@
+/*
+https://atcoder.jp/contests/abc214/tasks/abc214_b?lang=en
+*/
+#include <iostream>
+using namespace std;
+
+int countTriples(int S, int T)
+{
+    int count = 0;
+
+    for (int a = 0; a <= S; a++)
+        for (int b = 0; b <= S - a; b++)
+            for (int c = 0; c <= S - a - b; c++)
+                if (a * b * c <= T)
+                    count++;
+
+    return count;
+}
+
+int main()
+{
+    int S, T;
+    cin >> S >> T;
+
+    int result = countTriples(S, T);
+    cout << result << endl;
+
+    return 0;
+}
