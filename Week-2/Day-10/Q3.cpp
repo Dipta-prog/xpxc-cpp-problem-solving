e8857c89cad3934b3c401626927f17a22b45b526
Dipta-prog <anotheramit8@gmail.com>
2023-06-11 22:29:44 +0600

    - Q1 440A - Forgotten Episode Solved

---
+++ Week-2/Day-10/Q3.cpp
@@ -0,0 +1,26 @@
+/*
+https://codeforces.com/problemset/problem/440/A
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    bool a[100000];
+    memset(a, false, sizeof(a));
+    int n, x, i;
+    cin >> n;
+    i = 0;
+    
+    while (i < n - 1)
+    {
+        cin >> x;
+        a[x] = true;
+        i++;
+    }
+    for (i = 1; a[i] && i < n; i++)
+        ;
+
+    cout << i << "\n";
+    return 0;
+}
