60fc1e1a00b59f60dcff4610798f36a4888783cf
dipta-sikder-775 <anotheramit8@gmail.com>
2023-06-21 20:56:05 +0600

    - Q1 Donation Drive Solved

---
+++ Week-3/Day-20/codechef_contest/Q1.cpp
@@ -0,0 +1,20 @@
+/*
+https://www.codechef.com/START95D/problems/DONDRIVE
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int N, X;
+        cin >> N >> X;
+        cout << N - X << endl;
+    }
+
+    return 0;
+}
