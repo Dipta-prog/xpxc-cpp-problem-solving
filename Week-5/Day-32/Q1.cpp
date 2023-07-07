8a069bf61bda027e3833fc80712cd84f0ac49c61
dipta-sikder-775 <anotheramit8@gmail.com>
2023-07-07 22:31:38 +0600

    - Q1 A - Number Transformation Solved

--- Week-5/Day-32/Q1.cpp
+++ Week-5/Day-32/Q1.cpp
@@ -1,4 +1,37 @@
 /*
-
+https://codeforces.com/contest/1674/problem/A
 */
+#include <iostream>
+using namespace std;
 
+void solve()
+{
+    long long int x, y;
+    cin >> x >> y;
+
+    if (y % x == 0)
+    {
+        cout << 1 << " " << y / x << "\n";
+        return;
+    }
+    else
+    {
+        cout << 0 << " " << 0 << "\n";
+        return;
+    }
+}
+
+int main()
+{
+    ios ::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    long long int t = 1;
+    cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}
