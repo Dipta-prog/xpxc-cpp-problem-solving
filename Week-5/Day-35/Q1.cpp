37c11c84abdfa748e1c0a31f3f27e334066cfe3f
dipta-sikder-775 <anotheramit8@gmail.com>
2023-07-10 19:29:36 +0600

    - Q1 A - Chips Moving Solved

--- Week-5/Day-35/Q1.cpp
+++ Week-5/Day-35/Q1.cpp
@@ -1,4 +1,22 @@
 /*
-
+https://codeforces.com/contest/1213/problem/A
 */
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int n;
+    cin >> n;
+    int change_to = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        int x;
+        cin >> x;
+        change_to += x & 1;
+    }
+
+    cout << min(change_to, n - change_to) << "\n";
+
+    return 0;
+}
