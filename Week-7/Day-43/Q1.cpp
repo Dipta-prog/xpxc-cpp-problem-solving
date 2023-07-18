65d27d596cccd32f6b394ffcb4b6d4c80b7a5760
dipta-sikder-775 <anotheramit8@gmail.com>
2023-07-18 22:17:11 +0600

    Solved

--- Week-7/Day-43/Q1.cpp
+++ Week-7/Day-43/Q1.cpp
@@ -2,3 +2,25 @@
 
 */
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, M;
+        cin >> N >> M;
+
+        int maxRuns = M * 6;
+        if (N <= maxRuns || (N <= maxRuns + 6 && N % 6 == 0)) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
+
