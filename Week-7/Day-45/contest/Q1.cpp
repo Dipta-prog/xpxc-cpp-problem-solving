190381f66917ebc2e0309b59e6dbc732054ab614
dipta-sikder-775 <anotheramit8@gmail.com>
2023-07-19 21:32:02 +0600

    Q1 Puzzle Hunt Solved

---
+++ Week-7/Day-45/contest/Q1.cpp
@@ -0,0 +1,15 @@
+/*
+https://www.codechef.com/START99D/problems/PUZHUNT
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    (N >= 6 && N <= 8) ? cout << "Yes\n" : cout << "No\n";
+
+    return 0;
+}
